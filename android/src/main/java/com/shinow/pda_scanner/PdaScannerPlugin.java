bc383f9a59e2c4fe1d9c528581b5c8b3d4ec471c
yanningyang <ksn_cn@qq.com>
2019-10-25 10:38:04 +0800

    Update PdaScannerPlugin.java

--- android/src/main/java/com/shinow/pda_scanner/PdaScannerPlugin.java
+++ android/src/main/java/com/shinow/pda_scanner/PdaScannerPlugin.java
@@ -3,11 +3,14 @@ package com.shinow.pda_scanner;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import java.util.Set;
 
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.PluginRegistry;
